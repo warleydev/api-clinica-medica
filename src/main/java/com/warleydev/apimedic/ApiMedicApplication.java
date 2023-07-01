a88faad3f95b7936f14d8eb0512f37ec7596c789
Warley Dev <warleycm7@gmail.com>
2023-07-01 18:53:47 -0300

    projeto iniciado

---
+++ src/main/java/com/warleydev/apimedic/ApiMedicApplication.java
@@ -0,0 +1,13 @@
+package com.warleydev.apimedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warleydev/apimedic/ApiMedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warleydev.apimedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
