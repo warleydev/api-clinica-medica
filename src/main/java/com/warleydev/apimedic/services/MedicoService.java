01a4a3ef4145f03933d782c5cf1a755edfad3dd6
Warley Dev <warleycm7@gmail.com>
2023-07-03 20:05:25 -0300

    ajustes

--- src/main/java/com/warleydev/apimedic/services/MedicoService.java
+++ src/main/java/com/warleydev/apimedic/services/MedicoService.java
@@ -4,12 +4,17 @@ import com.warleydev.apimedic.dto.CadastroMedicos;
 import com.warleydev.apimedic.entities.Medico;
 import com.warleydev.apimedic.repositories.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class MedicoService {
 
     @Autowired
     MedicoRepository repository;
 
+
+    @Transactional
     public CadastroMedicos save(Medico entidade){
         entidade = repository.save(entidade);
         return new CadastroMedicos(entidade);
